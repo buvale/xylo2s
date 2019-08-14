66e46ffa04d2d4caade912d57a1af4f34ff73add
buvale <valentina.butto@gmail.com>
2019-08-14 16:35:08 +0700

    test2

---
+++ R/tidy_UP.R
@@ -0,0 +1,12 @@
+#tyding up stuff
+
+
+tidy_UP<- function(data,ID,HP,Y) {
+
+data.table::as.data.frame(setDT(data)[,shp := cut(HP, unique(quantile(HP, seq(0, 1, by= 0.01))),
+                             labels = FALSE, include.lowest = TRUE), by = .(ID,Y) ])
+
+}
+
+prompt(tidy_UP)
+

7153b996ac2c06c882e5762ff7463b3d6149266e
buvale <valentina.butto@gmail.com>
2019-08-14 16:16:19 +0700

    cell row organizer

---
+++ tidy_UP.R
@@ -0,0 +1,14 @@
+#tyding up stuff
+require(data.table)
+
+
+tidy_UP<- function(data,ID,HP,Y) {
+
+as.data.frame(setDT(data)[,shp := cut(HP, unique(quantile(HP, seq(0, 1, by= 0.01))),
+                             labels = FALSE, include.lowest = TRUE), by = .(ID,Y) ])
+
+}
+
+prompt(tidy_UP)
+
+#bipbip

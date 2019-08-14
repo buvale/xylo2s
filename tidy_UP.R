5c96f26b652885f9bae3b71c528b2cddb1e00b7f
buvale <valentina.butto@gmail.com>
2019-08-14 16:24:43 +0700

    mod

--- tidy_UP.R
+++ tidy_UP.R
@@ -1,10 +1,10 @@
 #tyding up stuff
-require(data.table)
+
 
 
 tidy_UP<- function(data,ID,HP,Y) {
 
-as.data.frame(setDT(data)[,shp := cut(HP, unique(quantile(HP, seq(0, 1, by= 0.01))),
+data.table::as.data.frame(setDT(data)[,shp := cut(HP, unique(quantile(HP, seq(0, 1, by= 0.01))),
                              labels = FALSE, include.lowest = TRUE), by = .(ID,Y) ])
 
 }

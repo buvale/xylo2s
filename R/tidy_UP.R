e26c5240c7f14f788f15ce39ae55b7b3dc086add
buvale <valentina.butto@gmail.com>
2019-08-15 11:05:51 +0700

    commit

--- R/tidy_UP.R
+++ R/tidy_UP.R
@@ -1,15 +1,13 @@
 #tyding up stuff
-require(data.table)
+require(dplyr)
 
 tidy_UP<- function(data,HP,ID,Y) {
 
      data %>%
      group_by(!!Y,!!ID) %>%
-     mutate(shp := !!ntile(HP,100))
+     dplyr::mutate(shp := !!ntile(HP,100))
 
 }
-
-
 
 
 
